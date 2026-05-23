b29903ba99c4fef3a345686a192d63850ca0458c
ameesha <amisha.bobbili@gmail.com>
2026-05-23 21:35:38 +0530

    practice

--- mysqll.sh
+++ mysqll.sh
@@ -30,7 +30,7 @@ userid=$(id -u) # <-- Added this to get the current user ID
 
 CHECK_ROOT
  mkdir -p "$LOGS_FOLDER"
- dnf install mysql-server -y 
+ dnf install -y https://mysql.com 
  VALIDATE $? "installing mysql server" 
 
  systemctl enable mysqld  
