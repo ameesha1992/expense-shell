243b838ce18e5f03ef796e467484d089f58140f7
ameesha <amisha.bobbili@gmail.com>
2026-05-23 20:15:11 +0530

    practice

--- mysqll.sh
+++ mysqll.sh
@@ -6,7 +6,7 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
 userid=$(id -u) # <-- Added this to get the current user ID
-sudo mkdir -p "$LOGS_FOLDER"
+ mkdir -p "$LOGS_FOLDER"
 
  CHECK_ROOT(){
   if [ $userid -ne 0 ]
@@ -29,7 +29,7 @@ sudo mkdir -p "$LOGS_FOLDER"
   } 
 
 CHECK_ROOT
-
+ mkdir -p "$LOGS_FOLDER"
  dnf install mysql-server -y &>>$LOG_FILE
  VALIDATE $? "installing mysql server" 
 
@@ -39,5 +39,6 @@ CHECK_ROOT
  systemctl start mysqld  &>>$LOG_FILE
  VALIDATE $? "starting mysql server"
  
- mysql_secure_installation --set-root-pass expenseapp@1 &>>$LOG_FILE
- VALIDATE $? "setting root_password"
+ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'expenseapp@1'; FLUSH PRIVILEGES;" &>>"$LOG_FILE"
+ VALIDATE $? "Configuring secure root password"
+
