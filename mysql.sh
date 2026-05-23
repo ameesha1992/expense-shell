634009ed6213eba161c5ddb60aded82c04972ca5
ameesha <amisha.bobbili@gmail.com>
2026-05-23 14:50:02 +0530

    practice

--- mysql.sh
+++ mysql.sh
@@ -5,7 +5,7 @@ LOGS_FOLDER="/var/log/expense-shell/"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1) 
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
- sudo mkdir -p $LOGS_FOLDER
+sudo mkdir -p $LOGS_FOLDER
 
  CHECK_ROOT(){
   if [$userid -ne 0]
