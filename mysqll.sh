f168eead7b62fc775bd296a14d0037a35924af94
ameesha <amisha.bobbili@gmail.com>
2026-06-02 16:16:28 +0530

    practice

--- mysqll.sh
+++ mysqll.sh
@@ -1,48 +1,52 @@
 #!/bin/bash
 
-
-LOGS_FOLDER="/var/log/expense-shell/"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1) 
-TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
- CHECK_ROOT(){
-  if [ $userid -ne 0 ]
-  then
-    echo "please run this script with root previlages"|tee -a $LOG_FILE
-    exit 1
-  else
-    echo "the script was already in root.." | tee -a $LOG_FILE
-  fi
-  }
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
-   VALIDATE(){
-    if [ $1 -ne 0 ]
-     then
-      echo "$2 is failed.." | tee -a $LOG_FILE
-      exit 1
-    else
-       echo "$2 is success.." | tee -a $LOG_FILE
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
     fi
-    }
-  echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
- dnf install mysql-server -y
- VALIDATE $? "installing mysql-server" 
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Server"
 
- systemctl enable mysqld  
- VALIDATE $? "enabling mysql server"
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabled MySQL Server"
 
- systemctl start mysqld  
- VALIDATE $? "starting mysql server"
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Started MySQL server"
 
- mysql -h amisha.site -u root -pexpenseapp@1 -e "show databases"
- if [$? ne 0]
- then
-  echo "mysql root password not setup try to set"
- else
-  echo "mysql root had already setup..skipping"
- fi
+#  mysql -h amisha.site -u root -pexpenseapp@1 -e "show databases"
+#  if [$? ne 0]
+#  then
+#   echo "mysql root password not setup try to set"
+#  else
+#   echo "mysql root had already setup..skipping"
+#  fi
